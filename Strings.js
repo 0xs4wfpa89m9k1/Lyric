7ad474588bb537818ba3fe0a69270869a80164d1
Lyric <jimmyjordan1234567@outlook.com>
2024-07-16 22:50:28 -0400

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,15 @@
+[
+    "I'm gonna kill myself",
+    "I'm gay",
+    "penis123",
+    "i have a chode",
+    "Chode: A chode is a penis that is wider than it is longer",
+    "Clams Casino is the best producer💯💯💯💯💯",
+    "Hawk Tuah: The most annoying thing ever said.",
+    "https://x.com/dreamybullxxx",
+    "i put more time into this shitty list of dead memes then i did into the obfuscator itself",
+    "i copied most of these quotes from synapse xen",
+    "2.5 teribytes of gay porn",
+    "Luraph is 🏳️‍🌈",
+    "FAT CHODE🐓"
+]
